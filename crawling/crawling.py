61518cae32c917021ee9698bfac87f7ff39f85a7
dewble <dewble62@gmail.com>
2022-02-10 23:51:01 +0900

    add crawling test

---
+++ crawling/crawling.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+
+
+def crawling(url, css_selector):
+    return_data = list()
+    res = requests.get(url)
+    soup = BeautifulSoup(res.content, 'html.parser')
+    datas1 = soup.select(css_selector)
+    for item in datas1:
+        return_data.append(item.get_text())
+    return return_data
+
+
+
+res = requests.get('https://dewble.com')
+print("### res.headers")
+print(res.headers['content-type'])
+
+print()
+print("### crawling method")
+result = crawling('https://dewble.com','h2')
+print(result)
